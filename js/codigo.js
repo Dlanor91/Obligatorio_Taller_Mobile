3151491dbfd822362b3977df0cbca8d70ad165e7
Ronald Lima Nunez <limaronald926@gmail.com>
2022-02-10 01:45:23 -0300

    Registro

---
+++ js/codigo.js
@@ -0,0 +1,64 @@
+/* Funcion Registro */
+document.querySelector("#btnRegistro").addEventListener("click", regsitroUsuario);
+
+
+function regsitroUsuario(){
+    let userRegister = document.querySelector("#userRegister").value.trim();
+    let paswRegister = document.querySelector("#paswRegister").value.trim();
+
+    /* Creo mi arreglo de datos para pasar al sistema */
+    let datosIngresados ={
+        "usuario": userRegister, /* el de "" es el de el post y el otro la variable q le igualo */
+        "password":paswRegister
+    }
+   
+    /* Validacion de JS */
+    try {
+        if(userRegister===""){
+            throw new Error("Complete el campo de Usuario.");
+        }
+        if(paswRegister===""){
+            throw new Error("Complete el campo de Contraseña.");
+        }
+
+        fetch("https://envios.develotion.com/usuarios.php",
+        {
+            method: "POST",
+            body:JSON.stringify(datosIngresados), /* Aqui paso el arreglo de datos para el post */
+            headers:{
+                "content-type":"application/json"
+            }
+        })
+        .then(function(response){
+            
+            if(response.status === 409){
+                throw new Error("Usuario existente, registrese con otro nombre de usuario.");
+            }
+            if(response.status != 200){
+                throw new Error("Ingrese los datos correctamente.");
+            }
+            return response.json();
+        })
+        .then(function (data){ 
+            console.log(data.apiKey);                
+        })
+        .catch(function(Error){
+           handleButtonClick(Error); 
+        })
+
+    } catch (error) {        
+        handleButtonClick(error);
+    }  
+}
+
+async function handleButtonClick(showError) {
+    const toast = await toastController.create({
+      color: 'dark',
+      duration: 2000,
+      message: showError.message,
+      showCloseButton: true,
+    });
+
+    await toast.present();            }
+
+ 
